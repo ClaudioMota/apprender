b142a47c44c97b00257c4f066d631d8bc9c5956c
despatrick <patricksandes38@gmail.com>
2021-05-23 10:38:29 -0300

    Adicionado o comando Add a classes

--- android/app/src/main/java/zuera/apprender/command.kt
+++ android/app/src/main/java/zuera/apprender/command.kt
@@ -12,4 +12,28 @@ class Classes : Command() {
         classesExample()
         return ""
     }
+
+
 }
+class Add : Command() {
+    override fun run(params :List<String>) : String
+    {
+        if(params.isEmpty()) return "Something is wrong!"
+
+        val parseInt = params.map {
+            it.toIntOrNull()
+        }
+
+        var add = 0
+
+        for (i in parseInt) {
+            if (i == null) return "You didn't insert acceptable values!"
+            else
+                add += i
+        }
+
+        return add.toString()
+    }
+    }
+
+
